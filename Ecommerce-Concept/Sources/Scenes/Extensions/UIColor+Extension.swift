c9bd2b0cdfc2a12063833f6dd458e994ff478c59
Viktor <viktopiniaginya@gmail.com>
2022-12-17 13:26:56 +0300

    add fonts and UIFonts extension

--- Ecommerce-Concept/Sources/Scenes/Extensions/UIColor+Extension.swift
+++ Ecommerce-Concept/Sources/Scenes/Extensions/UIColor+Extension.swift
@@ -7,11 +7,7 @@
 
 import UIKit.UIColor
 
-typealias AppColor = UIColor.CustomColors
-
 extension UIColor {
-    public enum CustomColors {
-        static let orange: UIColor = #colorLiteral(red: 1, green: 0.5189241767, blue: 0.3763272166, alpha: 1)
-        static let darkBlue: UIColor = #colorLiteral(red: 0.003921568627, green: 0, blue: 0.2078431373, alpha: 1)
-    }
+    static let orange: UIColor = #colorLiteral(red: 1, green: 0.5189241767, blue: 0.3763272166, alpha: 1)
+    static let darkBlue: UIColor = #colorLiteral(red: 0.003921568627, green: 0, blue: 0.2078431373, alpha: 1)
 }

a85f47e5f359ecc60401bb4fa72d7be2426dea80
Viktor <viktopiniaginya@gmail.com>
2023-01-09 14:31:34 +0300

    add addAddress UIs

--- Ecommerce-Concept/CartViewController.swift
+++ Ecommerce-Concept/CartViewController.swift
@@ -19,6 +19,12 @@ class CartViewController: UIViewController {
                                                objectColor: nil,
                                                backgroundView: .roundedCorner(color: .customDarkBlue),
                                                isBackButton: true)
+    private var addAddressButton = CustomButton(internalObject: UIImage(named: "addAddress"),
+                                                objectColor: .white,
+                                                backgroundView: .roundedCorner(color: .customOrange))
+    private lazy var addAddressLabel = UILabel(constant: Strings.addAddressLabelText,
+                                              with: Metrics.addAddressLabelTextSize,
+                                              and: .medium, .customDarkBlue)
     
     // MARK: - Lifecycle
     
@@ -33,15 +39,30 @@ class CartViewController: UIViewController {
     // MARK: - Settings
     
     private func setupHierarchy() {
-        view.addSubviews(backButton)
+        view.addSubviews(backButton, addAddressButton, addAddressLabel)
     }
     
     private func setupLayout() {
         NSLayoutConstraint.activate([
+            // MARK: -  backButton constraints
             backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: Metrics.backButtonTopInset),
             backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.backButtonLeadingInset),
-            backButton.heightAnchor.constraint(equalToConstant: Metrics.backButtonMainSize),
-            backButton.widthAnchor.constraint(equalToConstant: Metrics.backButtonMainSize)
+            backButton.heightAnchor.constraint(equalToConstant: Metrics.titleButtonsMainSize),
+            backButton.widthAnchor.constraint(equalToConstant: Metrics.titleButtonsMainSize),
+            
+            // MARK: - addAddressButton constraints
+            addAddressButton.centerYAnchor.constraint(equalTo: backButton.centerYAnchor),
+            addAddressButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Metrics.addAddressButtonTrailingInset),
+            addAddressButton.heightAnchor.constraint(equalToConstant: Metrics.titleButtonsMainSize),
+            addAddressButton.widthAnchor.constraint(equalToConstant: Metrics.titleButtonsMainSize),
+            
+            // MARK: -  addAddressLabel constraints
+            addAddressLabel.centerYAnchor.constraint(equalTo: backButton.centerYAnchor),
+            addAddressLabel.trailingAnchor.constraint(equalTo: addAddressButton.leadingAnchor, constant: Metrics.addAddressLabelTrailingInset),
+            addAddressLabel.heightAnchor.constraint(equalToConstant: Metrics.addAddressLabelHeight),
+            addAddressLabel.widthAnchor.constraint(equalToConstant: Metrics.returnLabelsWidth(text: addAddressLabel.text!,
+                                                                                              with: 11,
+                                                                                              and: .markProMedium(ofSize: Metrics.addAddressLabelTextSize)))
         ])
     }
     
@@ -57,10 +78,22 @@ extension CartViewController {
     enum Metrics {
         static let backButtonTopInset: CGFloat = 79
         static let backButtonLeadingInset: CGFloat = 42
-        static let backButtonMainSize: CGFloat = 37
+        static let titleButtonsMainSize: CGFloat = 37
+        
+        static let addAddressButtonTrailingInset: CGFloat = -48
+        
+        static let addAddressLabelTextSize: CGFloat = 15
+        static let addAddressLabelTrailingInset: CGFloat = -9
+        static let addAddressLabelHeight: CGFloat = 19
+        
+        static func returnLabelsWidth(text: String,
+                                      with constrainedHeight: CGFloat,
+                                      and font: UIFont) -> CGFloat {
+            return text.width(withConstrainedHeight: constrainedHeight, font: font)
+        }
     }
     
     enum Strings {
-        
+        static let addAddressLabelText = "Add address"
     }
 }
